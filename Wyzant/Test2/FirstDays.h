32f179f67b2b3006d6d8480a48977619174231d4
matteblackrobotics <mkpfeiffer202@gmail.com>
2023-03-28 17:46:51 -0400

    Update FirstDays.h

--- Wyzant/Test2/FirstDays.h
+++ Wyzant/Test2/FirstDays.h
@@ -10,7 +10,7 @@
 // day of year of first days of each month
 // const int firstDay[12] = {1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335};
 const int monthLengths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-int firstDays[12];  // dayOfYear for first day of every month
+int firstDays[12];  // dayOfYear for first days of every month
 int firstDayColors[3] = {0, 40, 40}; // RGB
 
 
