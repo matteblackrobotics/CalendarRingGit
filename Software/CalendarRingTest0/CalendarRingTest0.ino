fc4b7e2a7980bab004faf6d47bfa51a1a322206d
matteblackrobotics <mkpfeiffer202@gmail.com>
2023-04-05 21:49:00 -0400

    Added basic code

---
+++ Software/CalendarRingTest0/CalendarRingTest0.ino
@@ -0,0 +1,39 @@
+// Notes:
+// Serial printing fails dotStar
+// Only limited use serial.print() allows the dotStar to update
+// Uncommenting line DotStar.h 43 for example prevents dotStar from updating.
+
+
+#include "xPrint.h"
+#include "FirstDays.h" 
+#include "DotStar.h"
+
+// ---------------------- setup -------------------- //
+void setup() 
+{
+  setupSerial();
+
+  // --------- dotStar -------- //
+  setupDotStar();
+  setFirstDay();
+  setFinalDay();
+  setFinalLED();
+  //stripTest();
+  
+  // ---------- firstDays -------- //
+  calculateFirstDays();
+  //printFirstDays();
+  //printFirstDayColors();
+  setFirstDays();
+  Serial.println();
+
+  showStrip1();
+}
+
+// ---------------------- loop -------------------- //
+void loop() 
+{
+  //Serial.print("millis(): ");
+  //Serial.println(millis());
+  delay(1000);
+}
